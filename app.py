cc9ff319a16e5c15e90233a740b427930a6a6366
Mosap Abdel-Ghany <abdelghanymosap@gmail.com>
2025-12-29 21:18:30 +0200

    Refactor Car Price Prediction API implementation
    Refactor code to load model once and update input schema.

--- app.py
+++ app.py
@@ -3,20 +3,17 @@ from pydantic import BaseModel
 import pandas as pd
 import pickle
 
-app = FastAPI(title="Car Price Prediction API")
-
-# load artifacts once
-encoders = pickle.load(open("label_encoders.pkl", "rb"))
-scaler = pickle.load(open("scaler.pkl", "rb"))
+# load model once
 cat = pickle.load(open("catboost.pkl", "rb"))
 
+app = FastAPI(title="Car Price Prediction API")
 
+# input schema
 class CarInput(BaseModel):
     year: int
+    make: str
     model: str
-    cab: str
     level: str
-    extra: str
     transmission: str
     cylinders: int
     disp_liters: float
@@ -25,8 +22,6 @@ class CarInput(BaseModel):
     fuel_type: str
     disclosures: bool
     as_is: bool
-    make: str
-
 
 def price_with_dynamic_range(predicted_price: float):
     if predicted_price <= 20000:
@@ -38,27 +33,19 @@ def price_with_dynamic_range(predicted_price: float):
 
     return {
         "predicted_price": round(predicted_price),
-        "price_range": f"{round(predicted_price - delta)} : {round(predicted_price + delta)}",
+        "price_range": f"{round(predicted_price - delta)} - {round(predicted_price + delta)}",
         "delta": delta
     }
 
-
 @app.post("/predict")
 def predict_price(data: CarInput):
-    df = pd.DataFrame([data.dict()])
+    manual_df = pd.DataFrame([data.dict()])
 
-    for col, le in encoders.items():
-        if col in df.columns:
-            df[col] = df[col].astype(str)
-            df[col] = df[col].map(
-                lambda x: le.transform([x])[0] if x in le.classes_ else -1
-            )
+    manual_df = manual_df.reindex(
+        columns=cat.feature_names_,
+        fill_value=0
+    )
 
-    num_cols = scaler.feature_names_in_
-    df[num_cols] = scaler.transform(df[num_cols])
+    predicted_price = cat.predict(manual_df)[0]
 
-    df = df.reindex(columns=cat.feature_names_, fill_value=0)
-
-    prediction = cat.predict(df)[0]
-
-    return price_with_dynamic_range(prediction)
+    return price_with_dynamic_range(predicted_price)
